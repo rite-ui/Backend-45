d22ce088d2dedbd32f5f4d82a5f7defac55f378a
rite-ui <riteshranjan10705@gmail.com>
2026-02-13 18:47:20 +0530

    added

--- authentication/stateful/src/routes/auth.routes.js
+++ authentication/stateful/src/routes/auth.routes.js
@@ -1,13 +1,13 @@
 
-import { Router } from "express";
+import {Router} from "express";
 import { getCurrentUser, login, logout, register } from "../controllers/auth.controllers.js";
-
+import {isAuthenticated} from "../middleware/auth.middleware.js"
 
 const router = Router();
 
 router.post("/register" , register)
-// router.post("/login" , login)
-// router.post("/logout" , isAuthenticated , logout)
-// router.get("/me" , isAuthenticated ,logger ,  getCurrentUser)
+router.post("/login" , login)
+router.post("/logout" , isAuthenticated , logout)
+router.get("/me" , isAuthenticated ,  getCurrentUser)
 
 export default router;
