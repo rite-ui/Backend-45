37b162d7e3a9d9d1c989abbd538e2e20a741d575
rite-ui <riteshranjan10705@gmail.com>
2026-02-20 11:22:45 +0530

    done

--- docker/index.js
+++ docker/index.js
@@ -1,13 +1,16 @@
+
 import express from "express";
 import 'dotenv/config';
 
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-}); 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.get('/', (req, res) => {
+  res.send('Hello from Dockerized Node.js App!');
 });
+
+console.log(process.env.JWT_SECRET)
+
+app.listen(process.env.PORT , ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
